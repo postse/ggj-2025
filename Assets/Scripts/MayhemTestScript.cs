28691b268a68b652454bfc0bbb8263c5a350970f
Denver Massey <denmas22@live.unc.edu>
2025-01-24 18:56:17 -0500

    reorganizing

--- Assets/Scripts/MayhemTestScript.cs
+++ Assets/Scripts/MayhemTestScript.cs
@@ -5,7 +5,7 @@ public class TestScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Testing!");
     }
 
     // Update is called once per frame
