52dda8ee5054e426c9037d93e88ed4399ea26479
Simon Post <spost@captechventures.com>
2025-01-25 10:58:20 -0500

    fixed camera, added ShakeCamera function

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -6,19 +7,44 @@ public class CameraController : MonoBehaviour
 
     [SerializeField]
     private float cameraParallaxMultiplier = 2.0f;
+    private static float cameraShakeIntensity = 0.1f;
+
+    private static bool isShaking = false;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Rigidbody>();
-
-        // I don't know why we have to do this
-        gameObject.SetActive(false);
-        gameObject.SetActive(true);
     }
 
     void LateUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x / cameraParallaxMultiplier, player.transform.position.y / cameraParallaxMultiplier, transform.position.z);
+        Vector3 cameraPosition = new Vector3(player.transform.position.x / cameraParallaxMultiplier, player.transform.position.y / cameraParallaxMultiplier, transform.position.z);
+
+        if (isShaking)
+        {
+            cameraPosition.x += Random.Range(-1f * cameraShakeIntensity, cameraShakeIntensity);
+            cameraPosition.y += Random.Range(-1f * cameraShakeIntensity, cameraShakeIntensity);
+        }
+
+        transform.position = cameraPosition;
         transform.LookAt(player.transform.position / (cameraParallaxMultiplier * 2));
+    }
+
+    public static void ShakeCamera(float seconds, float intensity)
+    {
+        cameraShakeIntensity = intensity;
+        isShaking = true;
+        CameraController instance = FindAnyObjectByType<CameraController>();
+
+        if (instance != null)
+        {
+            instance.StartCoroutine(instance.StopShakeAfterDelay(seconds));
+        }
+    }
+
+    private IEnumerator StopShakeAfterDelay(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        isShaking = false;
     }
 }
