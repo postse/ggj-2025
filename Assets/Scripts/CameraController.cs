522726d70f6c40e5ffbc34f8a1703df3940d3b13
Simon Post <spost@captechventures.com>
2025-01-25 10:20:14 -0500

    camera looks at player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private Rigidbody player;
+
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Rigidbody>();
+
         // I don't know why we have to do this
         gameObject.SetActive(false);
         gameObject.SetActive(true);
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(player.transform.position / 5f);
     }
 }
